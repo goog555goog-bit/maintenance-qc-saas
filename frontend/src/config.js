f61d7b49f6a22fb7374c666429a3a604c4653cb5
goog555goog-bit <goog555goog@gmail.com>
2026-08-20 21:57:06 +0700

    update api url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -4,5 +4,5 @@
  */
 export const CONFIG = {
   // วาง Web App URL ของ Google Apps Script (ที่ลงท้ายด้วย /exec) ในเครื่องหมายคำพูดด้านล่างนี้
-  GAS_API_URL: "https://script.google.com/macros/s/ใส่_URL_ของคุณตรงนี้/exec"
+  GAS_API_URL: "https://script.google.com/macros/s/AKfycbxY2fBPAGMBNn2SGGADSmw_7h9BR6OyPP-QGVp310aAw6pTyoq-rpOZrvfXnps2KKQ/exec"
 };
